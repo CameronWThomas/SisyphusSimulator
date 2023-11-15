2a6ee686c94247cc99e10a16d17878567889df62
Connor <connor.h.thomas@gmail.com>
2023-11-15 17:59:30 -0500

    General logic in place for boulder movement and arm stuff

--- Assets/Scripts/BoulderMovement/BoulderDetector.cs
+++ Assets/Scripts/BoulderMovement/BoulderDetector.cs
@@ -15,9 +15,11 @@ public class BoulderDetector : MonoBehaviour
 
     public bool IsInRange { get; private set; }
     public bool BoulderOnLeft { get; private set; }
-    public float ApproachAnglePercent { get; private set; }
+    public float ContactAnglePercent { get; private set; }
+    public Vector3 ApproachingVelocity { get; private set; }
 
     private GameObject m_Boulder;
+    private float m_Height => GetComponent<CapsuleCollider>().height;
 
     // Start is called before the first frame update
     void Start()
@@ -59,7 +61,9 @@ public class BoulderDetector : MonoBehaviour
 
         IsInRange = true;
         BoulderOnLeft = hitAngle < 0;
-        ApproachAnglePercent = Mathf.Lerp(0f, 1f, Mathf.Abs(hitAngle) / (DetectionAngle / 2));
+        ContactAnglePercent = Mathf.Lerp(0f, 1f, Mathf.Abs(hitAngle) / (DetectionAngle / 2));
+        var approachSpeed = Vector3.Dot(-boulderDirection, m_Boulder.GetComponent<Rigidbody>().velocity);
+        ApproachingVelocity = approachSpeed <= 0 ? Vector3.zero : -1 * approachSpeed * boulderDirection;
     }
 
     private void OnDrawGizmos()
@@ -69,14 +73,30 @@ public class BoulderDetector : MonoBehaviour
         Handles.color = new Color(0, 0, 1, colorAlpha);
         if (IsInRange)
         {
-            colorAlpha = ApproachAnglePercent;
+            colorAlpha = ContactAnglePercent;
             Handles.color = BoulderOnLeft
                 ? new Color(1, 0, 0, colorAlpha)
                 : new Color(1, .5f, 0, colorAlpha);
         }
 
+        var forward = transform.forward;
+        var up = transform.up;
+        if (Physics.Raycast(transform.position, Vector3.down, out var hitInfo, m_Height)) //TODO check for ground tag?
+        {
+            up = hitInfo.normal;
+            var angle = -Mathf.Abs(90f - Vector3.Angle(forward, up));
+            forward = Quaternion.Euler(angle, 0f, 0f) * forward;
+        }
+
         var detectionAngleHalf = DetectionAngle / 2f;
-        Handles.DrawSolidArc(transform.position, transform.up, transform.forward, detectionAngleHalf, DetectionRadius);
-        Handles.DrawSolidArc(transform.position, transform.up, transform.forward, -detectionAngleHalf, DetectionRadius);
+        Handles.DrawSolidArc(transform.position, up, forward, detectionAngleHalf, DetectionRadius);
+        Handles.DrawSolidArc(transform.position, up, forward, -detectionAngleHalf, DetectionRadius);
+        Handles.DrawSolidArc(transform.position, transform.right, forward, -detectionAngleHalf, DetectionRadius);
+        Handles.DrawSolidArc(transform.position, transform.right, forward, detectionAngleHalf, DetectionRadius);
+
+        if (!m_Boulder.IsUnityNull())
+        {
+            Handles.DrawLine(m_Boulder.transform.position, m_Boulder.transform.position + ApproachingVelocity, 4f);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,32 +1,42 @@
 using Assets.Scripts;
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float ForceMultiplier = 10f;
-    public float RotationSpeed = 2f;
-    public float MaxSpeed = 5.0f;
-    public float m_RayDistance = 2f;
-    public float m_PushActivationAngle = 45f;
-    public float m_HoldBackAngle = 45f;
-    public GameObject Boulder;
+    private const float ConstantForceModifier = 750f;
+
+    public float MoveForceMultiplier = 50f;
+    public float RotationSpeed = 5f;
+    public float MaxSpeed = 6f;
+
+    public float BoulderForceModifier = 5f;
+
+    public float MaxCorrectiveVelocity = .45f;
+    public float OvercorrectionCounterStrength = 10f;
+
+    public float MinResistiveForceApproachSpeed = .1f;
+    public float ResistiveForceModifer = 20f;
 
     //TODO probably don't want to do this, but for now...
     public GameObject LeftHand;
     public GameObject RightHand;
 
-    private Rigidbody m_Rb;
-    private Rigidbody m_BounderRb;
-    private CameraReference cameraReference;
     private Vector3 m_MoveDir;
-    private bool m_PushingBoulder = false;
+
+    private Rigidbody m_Rb;
+
+    private GameObject m_Boulder;
+    private Rigidbody m_BoulderRb;
 
     private BoulderDetector m_BoulderDetector;
 
+    private CameraReference cameraReference;
     private Quaternion planarRotation2 => cameraReference.PlanarRotation2;
 
-    private float m_BoulderForceMultiplier => m_BounderRb.mass * 7;
+    private float m_Height => GetComponent<CapsuleCollider>().height;
+
 
     void Start()
     {
@@ -35,7 +45,8 @@ public class PlayerController : MonoBehaviour
         m_Rb = GetComponent<Rigidbody>();
         cameraReference = FindObjectOfType<CameraReference>();
 
-        m_BounderRb = Boulder.GetComponent<Rigidbody>();
+        m_Boulder = GameObject.FindGameObjectsWithTag("Boulder").First();
+        m_BoulderRb = m_Boulder.GetComponent<Rigidbody>();
         m_BoulderDetector = GetComponent<BoulderDetector>();
 
         LeftHand.SetActive(false);
@@ -51,97 +62,109 @@ public class PlayerController : MonoBehaviour
             ? Vector3.zero 
             : (planarRotation2 * moveInput).normalized;
 
-        BoulderPushing();
-        MovementAndRotation();
-    }
-
-
-    private void BoulderPushing()
-    {
-        // TODO Power of the force should take keys into account
-        //  - both hands add together for forward pushing power but decrease corrective power overall
-        //  - single hand should have normal pushing power but greater corrective power in that direction
-        //  - getting gassed based on how many hands in use and force boulder is applying
-
-        // TODO have state machine for pushign mode like what cam did
-
-        // TODO boulder should be hard to move without using hands. impossible?
-
-        // TODO cleanup and probably remove that cross product part and fix.
-
-        var rightHandPress = Input.GetKey(KeyCode.E);
         var leftHandPress = Input.GetKey(KeyCode.Q);
-        RightHand.SetActive(rightHandPress); //TODO probably inefficient
+        var rightHandPress = Input.GetKey(KeyCode.E);
         LeftHand.SetActive(leftHandPress);
+        RightHand.SetActive(rightHandPress);
 
-        var boulderDirection = (Boulder.transform.position - transform.position).normalized;
-        var boulderAngle = Vector3.Angle(transform.forward, boulderDirection);
-        var inBoulderPushDirection = boulderAngle < m_PushActivationAngle;
-        var boulderOnLeftSide = Vector3.Angle(Vector3.Cross(transform.forward, boulderDirection), transform.up) > 90;
-        //Debug.Log($"Boulder on left side? {boulderOnLeftSide}");
-        //Debug.Log($"Boulder on left side? {boulderOnLeftSide} boulderAngle={boulderAngle}");
-        
-        if (!inBoulderPushDirection ||
-            (!leftHandPress && !rightHandPress) ||
-            !(boulderOnLeftSide == leftHandPress || !boulderOnLeftSide == rightHandPress) ||
-            !Physics.Raycast(transform.position, boulderDirection, out var hitInfo, m_RayDistance) ||
-            !hitInfo.transform.gameObject == Boulder)
+        UpdateBoulderMovementInfo(leftHandPress, rightHandPress);
+        if (m_BoulderMovementInfo.IsPushing)
         {
-            m_PushingBoulder = false;
-            //Debug.Log($"Pushing boulder? {false}");
-
-            return;
-        }
-        //Debug.Log($"Pushing boulder? {true}");
-
-
-        m_PushingBoulder = leftHandPress || rightHandPress;
-
-        var correctionDirection = boulderOnLeftSide ? transform.right : -transform.right;
-        //TODO make it a slerp?
-        var correctionForce = m_BoulderForceMultiplier * ForceMultiplier * Time.fixedDeltaTime * correctionDirection * (boulderAngle / m_PushActivationAngle);
-
-        m_BounderRb.AddForce(correctionForce, ForceMode.Force);
-
-        var boulderVelocityDirection = m_BounderRb.velocity.normalized;
-        var boulderToPlayer = -boulderDirection;
-        var boulderApproaching = Vector3.Angle(boulderToPlayer, boulderVelocityDirection) < m_HoldBackAngle;
-        var boulderApproachingLeft = Vector3.Angle(Vector3.Cross(boulderToPlayer, boulderVelocityDirection), transform.up) > 90;
-
-        if (boulderApproaching &&
-            (boulderApproachingLeft == leftHandPress || !boulderApproachingLeft == rightHandPress))
-        {
-            var resistanceForce = m_BoulderForceMultiplier * ForceMultiplier * Time.fixedDeltaTime * boulderDirection * m_BounderRb.velocity.magnitude;
-            m_BounderRb.AddForce(resistanceForce, ForceMode.Force);
+            BoulderCorrection();
+            BoulderResistance();
         }
 
-        //TODO nudge the boulder towards in front of you
-
+        if (m_MoveDir != Vector3.zero)
+        {
+            Movement();
+            Rotation();
+        }        
     }
 
-    private void MovementAndRotation()
+    private void BoulderCorrection()
     {
-        if (m_MoveDir == Vector3.zero)
+        var movingAwayFromCenter = m_BoulderMovementInfo.CorrectionVelocity * m_BoulderMovementInfo.CorrectionModifier < 0f;
+        if (!(movingAwayFromCenter || m_BoulderMovementInfo.CorrectionVelocity < MaxCorrectiveVelocity))
         {
             return;
         }
 
-        var rigidBody = m_PushingBoulder ? m_BounderRb : m_Rb;
-        var extraForce = m_PushingBoulder ? m_BoulderForceMultiplier : 750;
+        var correctionModifier = movingAwayFromCenter ? OvercorrectionCounterStrength : 1f;
 
-        // Move
-        var moveForce = extraForce * ForceMultiplier * Time.fixedDeltaTime * m_MoveDir;
-        rigidBody.AddForce(moveForce, ForceMode.Force);
-        if (!m_PushingBoulder && m_Rb.velocity.magnitude > MaxSpeed)
+        var correctionForce = correctionModifier * ConstantForceModifier * m_BoulderMovementInfo.CorrectionModifier * Time.fixedDeltaTime * transform.right;
+        m_BoulderRb.AddForce(correctionForce, ForceMode.Impulse);
+    }
+
+    private void BoulderResistance()
+    {
+        Debug.Log($"Boulder vel={m_BoulderMovementInfo.Resistance.magnitude}");
+        if (m_BoulderMovementInfo.Resistance.magnitude < MinResistiveForceApproachSpeed)
+        {
+            return;
+        }
+
+        m_BoulderRb.AddForce(ResistiveForceModifer * m_BoulderMovementInfo.Resistance, ForceMode.Impulse);
+    }
+
+    private BoulderMovementInfo m_BoulderMovementInfo = new();
+    private void UpdateBoulderMovementInfo(bool leftHandPress, bool rightHandPress)
+    {
+        m_BoulderMovementInfo.IsPushing = m_BoulderDetector.IsInRange
+            && ((m_BoulderDetector.BoulderOnLeft && leftHandPress) || (!m_BoulderDetector.BoulderOnLeft && rightHandPress));
+
+        if (!m_BoulderMovementInfo.IsPushing)
+        {
+            return;
+        }
+
+        m_BoulderMovementInfo.BoulderOnLeft = m_BoulderDetector.BoulderOnLeft;
+
+        var sign = m_BoulderDetector.BoulderOnLeft ? 1 : -1;
+        m_BoulderMovementInfo.CorrectionModifier = Mathf.Pow(m_BoulderDetector.ContactAnglePercent, 1.5f) * sign;
+        m_BoulderMovementInfo.LeftHand = leftHandPress;
+        m_BoulderMovementInfo.RightHand = rightHandPress;
+
+        var localBoulderVelocity = transform.InverseTransformDirection(m_BoulderRb.velocity);
+        m_BoulderMovementInfo.CorrectionVelocity = localBoulderVelocity.x;
+        m_BoulderMovementInfo.Resistance = -1 * m_BoulderDetector.ApproachingVelocity;
+    }
+
+    private class BoulderMovementInfo
+    {
+        public bool IsPushing;
+        public float CorrectionModifier;
+        public bool BoulderOnLeft;
+        public bool LeftHand;
+        public bool RightHand;
+
+        public float CorrectionVelocity;
+        public Vector3 Resistance;
+    }
+
+    private void Movement()
+    {
+        var moveDir = m_MoveDir;
+        if (Physics.Raycast(transform.position, Vector3.down, out var hitInfo, m_Height)) //TODO check for ground tag?
+        {
+            var angle = -Mathf.Abs(90f - Vector3.Angle(transform.forward, hitInfo.normal));
+            moveDir = Quaternion.Euler(angle, 0f, 0f) * moveDir;
+        }
+
+        var moveForce = ConstantForceModifier * MoveForceMultiplier * Time.fixedDeltaTime * moveDir;
+
+        if (m_BoulderMovementInfo.IsPushing)
+        {
+            m_BoulderRb.AddForce(BoulderForceModifier * moveForce, ForceMode.Force);
+        }
+        m_Rb.AddForce(moveForce, ForceMode.Force);
+        if (m_Rb.velocity.magnitude > MaxSpeed)
         {
             m_Rb.velocity = m_Rb.velocity.normalized * MaxSpeed;
         }
-        else if (m_PushingBoulder)
-        {
-            m_Rb.velocity = rigidBody.velocity;
-        }
+    }
 
-        // Rotate
+    private void Rotation()
+    {
         var lookRotation = Quaternion.LookRotation(m_MoveDir);
         var targetRotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.fixedDeltaTime * RotationSpeed * 180);
         transform.rotation = targetRotation;
