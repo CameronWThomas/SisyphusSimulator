53a6060581a1238922efbfacf13b1a3046eb9e03
Connor <connor.h.thomas@gmail.com>
2023-11-29 21:16:31 -0500

    Improve boulder dropping, level and add a different material for the boulder obstacles

--- Assets/Scripts/BoulderDropper.cs
+++ Assets/Scripts/BoulderDropper.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,39 @@ using UnityEngine;
 public class BoulderDropper : MonoBehaviour
 {
     public GameObject boulderGroup;
+    public float timeTillReset = 60f;
 
     private Rigidbody[] boulders;
+    private List<Vector3> originalBoulderPositions;
     private bool triggered = false;
+    private float timeOfTrigger = 0f;
 
     private void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
         boulders = boulderGroup.GetComponentsInChildren<Rigidbody>();
+
+        originalBoulderPositions = new List<Vector3>();
+        foreach (var boulder in boulders)
+        {
+            originalBoulderPositions.Add(boulder.position);
+            boulder.transform.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (triggered && (Time.time - timeOfTrigger > timeTillReset))
+        {
+            triggered = false;
+            
+            for (int i = 0; i < boulders.Length; i++)
+            {
+                boulders[i].transform.position = originalBoulderPositions[i];
+                boulders[i].isKinematic = true;
+                boulders[i].transform.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,13 +48,13 @@ public class BoulderDropper : MonoBehaviour
             return;
         }
 
-        //TODO randomly drop them?
-
         foreach (var boulder in boulders)
         {
+            boulder.transform.GetComponent<MeshRenderer>().enabled = true;
             boulder.isKinematic = false;
         }
 
+        timeOfTrigger = Time.time;
         triggered = true;
     }
 }
